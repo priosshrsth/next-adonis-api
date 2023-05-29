394e462e9410342a8c57fe5f547c63711985a1e3
priosshrsth <shrsthprios@hotmail.com>
2023-05-30 00:32:41 +0545

    initial setup

---
+++ start/routes.ts
@@ -0,0 +1,34 @@
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| This file is dedicated for defining HTTP routes. A single file is enough
+| for majority of projects, however you can define routes in different
+| files and just make sure to import them inside this file. For example
+|
+| Define routes in following two files
+| ├── start/routes/cart.ts
+| ├── start/routes/customer.ts
+|
+| and then import them inside `start/routes.ts` as follows
+|
+| import './routes/cart'
+| import './routes/customer'
+|
+*/
+
+import Route from '@ioc:Adonis/Core/Route'
+import Database from '@ioc:Adonis/Lucid/Database'
+
+Route.get('/', async () => {
+
+  const users = await Database
+    .query()  // 👈 gives an instance of select query builder
+    .from('User')
+    .select('*')
+    .limit(5)
+
+  return users;
+
+})
